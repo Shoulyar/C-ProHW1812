71372fbff12a498f364f5e89234dd3f2a0b54369
Oleksandr Shuliar <shoulyar@live.com>
2023-12-28 20:37:40 +0100

    Third task added

---
+++ HW2OOPT3/DecimalNumber.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW2OOPT3
+{
+    public struct DecimalNumber
+    {
+        private int value;
+
+        public DecimalNumber(int value)
+        {
+            this.value = value;
+        }
+
+        public void ConvertToBinary()
+        {
+            string binary = Convert.ToString(value, 2);
+            Console.WriteLine($"Decimal {value} in binary: {binary}");
+        }
+
+        public void ConvertToOctal()
+        {
+            string octal = Convert.ToString(value, 8);
+            Console.WriteLine($"Decimal {value} in octal: {octal}");
+        }
+
+        public void ConvertToHexadecimal()
+        {
+            string hex = Convert.ToString(value, 16);
+            Console.WriteLine($"Decimal {value} in hexadecimal: {hex}");
+        }
+    }
+}
---
+++ HW2OOPT3/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace HW2OOPT3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DecimalNumber decimalNum = new DecimalNumber(123);
+
+            decimalNum.ConvertToBinary();        
+            decimalNum.ConvertToOctal();        
+            decimalNum.ConvertToHexadecimal(); 
+        }
+    }
+}
